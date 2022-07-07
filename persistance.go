51442077e651b183701c2d7dab10f0a91bbe1f09
direwolf05 <sabhya.1802@gmail.com>
2022-07-07 13:42:10 +0530

    Created Persistance class outline

---
+++ persistance.go
@@ -0,0 +1,12 @@
+package main
+
+type Persistance struct {
+}
+
+func (p *Persistance) store_reccos(map[string]map[string]string)
+
+func (p *Persistance) get_reccos() map[string]map[string]string
+
+func (p *Persistance) store_tagMap(map[string]string)
+
+func (p *Persistance) get_tagMap() map[string]string
