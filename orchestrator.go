9dbd3b26b4c125875c5b3c6d8328295b904376ef
anurodh-ti <anurodh.chadha@trilogy.com>
2022-07-08 16:06:04 +0530

    Added error to the return type of extract module function

--- orchestrator.go
+++ orchestrator.go
@@ -7,24 +7,26 @@ import (
 	"github.com/trilogy-group/cloudfix-linter/logger"
 )
 
-func main() {
-	//modulePaths := extractModulePaths(jsonString)
-}
-
-func extractModulePaths(jsonString string) []string {
+func extractModulePaths(jsonString string) ([]string, error) {
 	appLogger := logger.New()
+	var modulePaths []string
 	byteValue := []byte(jsonString)
 	/*
 		Initialising a variable result which stores the data in the format of defined structure.
 		Structure: "map(key->string,value->(array of map(key->string,value->interface))"
 	*/
 	var result map[string][]map[string]interface{}
-	json.Unmarshal([]byte(byteValue), &result)
+	err := json.Unmarshal([]byte(byteValue), &result)
+	if err != nil {
+		appLogger.Error().Println("Failed to unmarshall module paths from json string")
+		return modulePaths, err
+	}
+	appLogger.Info().Println("Unmarshalled module paths succesfully!")
 	noOfModules := len(result["issues"])
-	modulePaths := make([]string, noOfModules)
+	modulePaths = make([]string, noOfModules)
 	for key, element := range result["issues"] {
 		modulePaths[key] = fmt.Sprint(element["message"])
 	}
-	appLogger.Info().Println("Successfully extracted module paths from json string containing module paths")
-	return modulePaths
+	appLogger.Info().Println("Extracted module paths succesfully!")
+	return modulePaths, nil
 }
