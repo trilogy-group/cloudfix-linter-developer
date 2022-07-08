624a4d7bcd64c53d843c66361d9bd53044a73bf4
anurodh-ti <anurodh.chadha@trilogy.com>
2022-07-08 14:35:09 +0530

    Added Module path extractor directly from json string

--- orchestrator.go
+++ orchestrator.go
@@ -3,34 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/trilogy-group/cloudfix-linter/logger"
 )
 
 func main() {
-	_, err := extractModulePaths("sample.json")
-	if err != nil {
-		fmt.Println("Could not Extract module names:- ", err)
-		return
-	}
+	//modulePaths := extractModulePaths(jsonString)
 }
 
-func extractModulePaths(fileName string) ([]string, error) {
-	var emptyArray []string
+func extractModulePaths(jsonString string) []string {
 	appLogger := logger.New()
-	jsonFile, err := os.Open(fileName)
-	if err != nil {
-		appLogger.Error().Println("Error occurred while opening json file containing module names in orchestrator")
-		return emptyArray, err
-	}
-	defer jsonFile.Close()
-	byteValue, err := ioutil.ReadAll(jsonFile)
-	if err != nil {
-		appLogger.Error().Println("Error occurred while Reading from json file containing module names in orchestrator")
-		return emptyArray, err
-	}
+	byteValue := []byte(jsonString)
 	/*
 		Initialising a variable result which stores the data in the format of defined structure.
 		Structure: "map(key->string,value->(array of map(key->string,value->interface))"
@@ -42,5 +25,6 @@ func extractModulePaths(fileName string) ([]string, error) {
 	for key, element := range result["issues"] {
 		modulePaths[key] = fmt.Sprint(element["message"])
 	}
-	return modulePaths, nil
+	appLogger.Info().Println("Successfully extracted module paths from json string containing module paths")
+	return modulePaths
 }
