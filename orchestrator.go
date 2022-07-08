a17cc73ab440ecbb269f47a46254a4f600a5e57c
direwolf05 <sabhya.1802@gmail.com>
2022-07-08 12:18:08 +0530

    Added structures for parsing reccos json

--- orchestrator.go
+++ orchestrator.go
@@ -1,1 +1,30 @@
+package main
 
+type Parameter struct {
+	IdealType      string `json:"Migrating to instance type"`
+	RecentSnapshot bool   `json:"Has recent snapshot"`
+}
+
+type Response struct {
+	Id                     string
+	Region                 string
+	PrimaryImpactedNodeId  string
+	OtherImpactedNodeIds   []string
+	ResourceId             string
+	ResourceName           string
+	Difficulty             int
+	Risk                   int
+	ApplicationEnvironment string
+	AnnualSavings          float32
+	AnnualCost             float32
+	Status                 string
+	Parameters             Parameter
+	TemplateApproved       bool
+	CustomerId             int
+	AccountId              string
+	AccountNickname        string
+	OpportunityType        string
+	OpportunityDescription string
+	GeneratedDate          string
+	LastUpdatedDate        string
+}
