e28db9a0a9230333c6b0774c93e0c4eb76682a9f
Arpan Gupta <arpan.gupta@trilogy.com>
2022-11-30 15:56:28 +0530

    Referencing files based on OS

--- terraformManager.go
+++ terraformManager.go
@@ -3,14 +3,27 @@ package main
 import (
 	"os/exec"
 	"strings"
-
+	"os"
+	"runtime"
+	"path/filepath"
 	tfjson "github.com/hashicorp/terraform-json"
 )
 
 type TerraformManager struct {
 	//No data types required
 }
-
+// Giving reference to terraform.exe file if present in windows
+func terraform() string{
+	if(runtime.GOOS=="windows"){
+		ex, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+		basePath := filepath.Dir(ex)
+		return basePath+"\\terraform.exe"
+	}
+	return "terraform"
+}
 func (t *TerraformManager) getTagToID(TfLintOutData []byte) (map[string]string, error) {
 	tagToID := make(map[string]string)
 	var tfState tfjson.State
@@ -75,7 +88,7 @@ func (t *TerraformManager) addPairToTagMap(resource *tfjson.StateResource, tagTo
 
 func (t *TerraformManager) getTagToIDMapping() (map[string]string, error) {
 	tagToID := make(map[string]string)
-	TfLintOutData, errT := exec.Command("terraform", "show", "-json").Output()
+	TfLintOutData, errT := exec.Command(terraform(), "show", "-json").Output()
 	if errT != nil {
 		//Add Log
 		return tagToID, errT
