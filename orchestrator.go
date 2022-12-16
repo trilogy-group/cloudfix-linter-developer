81c5f06129557818671b2d554e24e593f80118e5
Arpan Gupta <arpan.gupta@trilogy.com>
2022-12-16 16:11:42 +0530

    changed file naming convention

--- orchestrator.go
+++ orchestrator.go
@@ -111,7 +111,7 @@ func (o *Orchestrator) runReccos(jsonFlag bool) {
 	var persist Persistance
 	var cloudfixMan cloudfixIntegration.CloudfixManager
 	var terraMan TerraformManager
-	reccosFileName := "recos.txt"
+	reccosFileName := "cloudfix-linter-recos.txt"
 	reccosMapping, errC := cloudfixMan.GetReccos()
 	if errC != nil {
 		logger.Info("Something went wrong. More logs in the log directory. ", errC)
@@ -131,7 +131,7 @@ func (o *Orchestrator) runReccos(jsonFlag bool) {
 		panic(errP)
 	}
 	os.Setenv("ReccosMapFile", reccosFileName)
-	tagFileName := "tagsID.txt"
+	tagFileName := "cloudfix-linter-tagsID.txt"
 	tagToIDMap, errG := terraMan.getTagToIDMapping()
 	if errG != nil {
 		panic(errG)
